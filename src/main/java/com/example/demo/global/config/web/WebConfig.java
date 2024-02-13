a92d7f31350a5d1f33e9671259fd3b3376c273ba
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-02-14 00:24:30 +0900

    [enh] Web & Security 기본 설정

---
+++ src/main/java/com/example/demo/global/config/web/WebConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo.global.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:8080/", "/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowCredentials(true);
+    }
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/docs/**")
+                .addResourceLocations("classpath:/static/docs/");
+    }
+}
