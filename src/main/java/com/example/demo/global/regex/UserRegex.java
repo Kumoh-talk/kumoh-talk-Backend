804aeba828075d5419e1cfcc0074e88e8bd297af
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-02-21 02:28:52 +0900

    [chore] Email 패턴 변경

--- src/main/java/com/example/demo/global/regex/UserRegex.java
+++ src/main/java/com/example/demo/global/regex/UserRegex.java
@@ -5,7 +5,7 @@ public class UserRegex {
     public static final String NAME_REGEXP = "^[가-힣a-zA-Z\\d\\[\\]<>-]{2,20}";
 
     // 일반적인 이메일 형식 체크
-    public static final String EMAIL_REGEXP = "[a-z0-9]+@[a-z]+\\.[a-z]{2,8}$";
+    public static final String EMAIL_REGEXP = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
 
     // 최소 8, 최대 25자 이내. 적어도 하나의 영문자와 하나의 숫자를 포함해야 한다.
     public static final String PASSWORD_REGEXP = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$";
