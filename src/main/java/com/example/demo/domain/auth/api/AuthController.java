473e2dca3a4ad32f75b313d590db87a55be0e513
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-02-21 16:26:24 +0900

    [chore] 공통 응답 객체 ApiResponse 제거

--- src/main/java/com/example/demo/domain/auth/api/AuthController.java
+++ src/main/java/com/example/demo/domain/auth/api/AuthController.java
@@ -3,10 +3,11 @@ package com.example.demo.domain.auth.api;
 import com.example.demo.domain.auth.application.AuthService;
 import com.example.demo.domain.auth.dto.request.JoinRequest;
 import com.example.demo.domain.auth.dto.request.ValidateEmailRequest;
-import com.example.demo.global.base.dto.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,16 +20,20 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping(value = "/sign-up", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ApiResponse<Void> join(@RequestBody @Valid JoinRequest request) {
-        Boolean joinSuccess = authService.join(request);
+    public ResponseEntity<Void> join(@RequestBody @Valid JoinRequest request) {
+        authService.join(request);
 
-        return ApiResponse.ok(joinSuccess);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .build();
     }
 
     @PostMapping("/duplicate")
-    public ApiResponse<Void> checkEmail(@RequestBody @Valid ValidateEmailRequest request) {
+    public ResponseEntity<Void> checkEmail(@RequestBody @Valid ValidateEmailRequest request) {
         boolean notExistEmail = authService.isNotExistEmail(request.getEmail());
 
-        return ApiResponse.ok(notExistEmail);
+        return ResponseEntity.
+                noContent()
+                .build();
     }
 }
