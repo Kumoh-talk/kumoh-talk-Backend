b78291f1db00ae85326718b5161d52e8e23be03e
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-02-23 02:33:10 +0900

    [enh] login 기능 추가

--- src/main/java/com/example/demo/domain/auth/application/AuthService.java
+++ src/main/java/com/example/demo/domain/auth/application/AuthService.java
@@ -1,10 +1,16 @@
 package com.example.demo.domain.auth.application;
 
 import com.example.demo.domain.auth.dto.request.JoinRequest;
+import com.example.demo.domain.auth.dto.request.LoginRequest;
+import com.example.demo.domain.auth.dto.response.LoginResponse;
 import com.example.demo.domain.user.domain.User;
 import com.example.demo.domain.user.repository.UserRepository;
+import com.example.demo.global.auth.token.application.TokenProvider;
 import com.example.demo.global.base.exception.ServiceException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +23,8 @@ import static com.example.demo.global.base.exception.ErrorCode.EXIST_SAME_EMAIL;
 public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final TokenProvider tokenProvider;
 
     @Transactional
     public void join(JoinRequest request) {
@@ -31,5 +39,15 @@ public class AuthService {
 
     public boolean isNotExistEmail(String email) {
         return !userRepository.existsByEmail(email);
+    }
+
+    public LoginResponse login(LoginRequest request) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        String accessToken = tokenProvider.createAccessToken(authentication);
+
+        return new LoginResponse(accessToken, "Bearer");
     }
 }
