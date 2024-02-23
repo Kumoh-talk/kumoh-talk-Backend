da2afce187ce3235028c29365a2b808b43e57bc7
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-02-23 17:52:13 +0900

    [chore] 테스트를 위한 기본생성자 추가

--- src/main/java/com/example/demo/domain/auth/dto/request/LoginRequest.java
+++ src/main/java/com/example/demo/domain/auth/dto/request/LoginRequest.java
@@ -2,11 +2,15 @@ package com.example.demo.domain.auth.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import static com.example.demo.global.regex.UserRegex.EMAIL_REGEXP;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     @NotBlank(message = "이메일을 입력해주세요.")
     @Pattern(regexp = EMAIL_REGEXP, message = "이메일 형식이 맞지 않습니다.")
