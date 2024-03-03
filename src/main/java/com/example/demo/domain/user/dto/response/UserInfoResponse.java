d692f42d533f8ce9eeb5a0b433e6b8b09d34e391
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-03-03 10:47:56 +0900

    [chore] 날짜 포맷 설정

--- src/main/java/com/example/demo/domain/user/dto/response/UserInfoResponse.java
+++ src/main/java/com/example/demo/domain/user/dto/response/UserInfoResponse.java
@@ -3,6 +3,7 @@ package com.example.demo.domain.user.dto.response;
 
 import com.example.demo.domain.user.domain.User;
 import com.example.demo.domain.user.domain.vo.Track;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -14,7 +15,9 @@ public class UserInfoResponse {
     private String email;
     private Track track;
     private String major;
+    @JsonFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     private LocalDateTime createdAt;
+    @JsonFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     private LocalDateTime updatedAt;
 
     public UserInfoResponse(User user) {
