0d4767de6f21263310be14c761ee41b89d522c5e
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-04-01 12:29:47 +0900

    refactor : exception 처리 수정

--- src/main/java/com/example/demo/global/base/exception/ErrorCode.java
+++ src/main/java/com/example/demo/global/base/exception/ErrorCode.java
@@ -7,49 +7,37 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
     // Common
-    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "잘못된 입력 값입니다."),
-    INVALID_TYPE_VALUE(HttpStatus.BAD_REQUEST, "잘못된 타입입니다."),
-    MISSING_INPUT_VALUE(HttpStatus.BAD_REQUEST,  "인자가 부족합니다."),
-    NOT_EXIST_API(HttpStatus.BAD_REQUEST, "요청 주소가 올바르지 않습니다."),
-    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "사용할 수 없는 메서드입니다."),
-    HANDLE_ACCESS_DENIED(HttpStatus.FORBIDDEN,  "접근 권한이 없습니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다."),
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "COMMON_0001", "잘못된 입력 값입니다."),
+    INVALID_TYPE_VALUE(HttpStatus.BAD_REQUEST, "COMMON_0002", "잘못된 타입입니다."),
+    MISSING_INPUT_VALUE(HttpStatus.BAD_REQUEST,"COMMON_0003", "인자가 부족합니다."),
+    NOT_EXIST_API(HttpStatus.BAD_REQUEST, "COMMON_0004", "요청 주소가 올바르지 않습니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "COMMON_0005", "사용할 수 없는 메서드입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_0006", "서버 에러입니다."),
+    INVALID_JSON(HttpStatus.BAD_REQUEST, "COMMON_0007", "JSON 파싱 오류입니다."),
+    NEED_AUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON_0008","인증이 필요합니다."),
 
-    // 400 error
-    INVALID_JSON(HttpStatus.BAD_REQUEST, "JSON 파싱 오류입니다."),
-    MISMATCH_EMAIL_OR_PASSWORD(HttpStatus.BAD_REQUEST, "이메일 혹은 비밀번호가 틀렸습니다."),
-    MISMATCH_EMAIL_AUTH_CODE(HttpStatus.BAD_REQUEST, "인증 코드가 틀렸습니다."),
 
-    // 401 error
-    NEED_AUTHORIZED(HttpStatus.UNAUTHORIZED,"인증이 필요합니다."),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "토큰의 유효성 혹은 형식이 올바르지 않습니다."),
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
-    NOT_AUTHORIZED_TOKEN(HttpStatus.UNAUTHORIZED, "권한 정보가 없는 토큰입니다."),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
-    IP_MISMATCH(HttpStatus.UNAUTHORIZED, "IP 주소가 기존과 일치하지 않습니다. 재로그인이 필요합니다."),
-    NOT_ACCESS_USER(HttpStatus.UNAUTHORIZED, "해당 유저가 접근할 수 없는 게시물입니다."),
+    // User
+    MISMATCH_EMAIL_OR_PASSWORD(HttpStatus.BAD_REQUEST, "USER_0001", "이메일 혹은 비밀번호가 틀렸습니다."),
+    MISMATCH_EMAIL_AUTH_CODE(HttpStatus.BAD_REQUEST, "USER_0002", "인증 코드가 틀렸습니다."),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "USER_0003", "비밀번호가 일치하지 않습니다."),
+    FAIL_USER_LOGIN(HttpStatus.NOT_FOUND, "USER_0004", "존재하지 않는 계정입니다."),
+    EXIST_SAME_EMAIL(HttpStatus.CONFLICT, "USER_0005", "이미 사용중인 이메일 입니다."),
+    UNABLE_TO_SEND_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "USER_0006","이메일을 전송할 수 없습니다."),
+    NO_SUCH_ALGORITHM(HttpStatus.INTERNAL_SERVER_ERROR, "USER_0007","해당 알고리즘을 찾을 수 없습니다."),
 
-    // 404 error
-    FAIL_USER_LOGIN(HttpStatus.NOT_FOUND, "존재하지 않는 계정입니다."),
-    COOKIE_REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "쿠키에 refresh 토큰 정보가 존재하지 않습니다."),
-    REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "refresh 토큰이 존재하지 않습니다. 재로그인이 필요합니다."),
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND,"존재하지 않는 게시물입니다." ),
+    // Board
+    NOT_ACCESS_USER(HttpStatus.UNAUTHORIZED, "BOARD_0001", "해당 유저가 접근할 수 없는 게시물입니다."),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "BOARD_0002", "존재하지 않는 게시물입니다." ),
 
-    // 409 error
-    EXIST_SAME_EMAIL(HttpStatus.CONFLICT, "이미 사용중인 이메일 입니다."),
+    // FILE
+    FILE_TOO_LARGE(HttpStatus.PAYLOAD_TOO_LARGE, "FILE_0001", "파일의 용량이 너무 큽니다."),
 
-    // 413 error
-    FILE_TOO_LARGE(HttpStatus.PAYLOAD_TOO_LARGE, "파일의 용량이 너무 큽니다."),
-
-    // 500 error
-    UNABLE_TO_SEND_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "이메일을 전송할 수 없습니다."),
-    NO_SUCH_ALGORITHM(HttpStatus.INTERNAL_SERVER_ERROR, "해당 알고리즘을 찾을 수 없습니다."),
-    UNABLE_TO_CREATE_AUTHENTICATION(HttpStatus.INTERNAL_SERVER_ERROR, "토큰에 정보가 부족하여 인증생성에 실패했습니다.");
-
+    ;
 
     private final HttpStatus status;
+    private final String code;
     private final String message;
 }
--- src/main/java/com/example/demo/global/base/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/global/base/exception/GlobalExceptionHandler.java
@@ -23,108 +23,78 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    private static final String EXCEPTION_FORMAT = "[EXCEPTION]                   -----> ";
-    private static final String EXCEPTION_MESSAGE_FORMAT = "[EXCEPTION] EXCEPTION_MESSAGE -----> [{}]";
-    private static final String EXCEPTION_TYPE_FORMAT = "[EXCEPTION] EXCEPTION_TYPE    -----> [{}]";
-    private static final String EXCEPTION_REQUEST_URI = "[EXCEPTION] REQUEST_URI       -----> [{}]";
-    private static final String EXCEPTION_HTTP_METHOD_TYPE = "[EXCEPTION] HTTP_METHOD_TYPE  -----> [{}]";
 
     @ExceptionHandler(ServiceException.class) // custom 에러
     public ResponseEntity<ErrorResponse> handleServiceException(HttpServletRequest request, ServiceException e) {
         ErrorCode errorCode = e.getErrorCode();
-        log.info("에러코드 테스트: " + String.valueOf(errorCode));
-        logService(request, e, errorCode);
-        return ErrorResponse.of(errorCode);
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(ErrorResponse.of(errorCode,null));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
-        log.debug("[EXCEPTION] FIELD_ERROR       -----> [{}]", e.getFieldError());
 
-        return ErrorResponse.of(BAD_REQUEST, errorMessage);
+        log.debug("MethodArgumentNotValidException", e);
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, errorMessage));
     }
 
-    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(MissingRequestValueException.class) // 요청 데이터로 들어와야할 인자 부족
     public ResponseEntity<ErrorResponse> handleMissingRequestValueException(HttpServletRequest request,
                                                             MissingRequestValueException e) {
-        logDebug(request, e);
-        return ErrorResponse.of(ErrorCode.MISSING_INPUT_VALUE);
+        log.debug("MissingRequestValueException", e);
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(ErrorCode.MISSING_INPUT_VALUE, null));
     }
 
-    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class) // 해당 uri에 잘못된 HttpMethod
     public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpServletRequest request,
                                                                       HttpRequestMethodNotSupportedException e) {
-        logDebug(request, e);
-        return ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED);
+        log.debug("HttpRequestMethodNotSupportedException", e);
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED, null));
     }
 
-    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(NoHandlerFoundException.class) // 없는 api(uri)
     public ResponseEntity<ErrorResponse> handleNoHandlerFoundException(HttpServletRequest request, NoHandlerFoundException e) {
-        logDebug(request, e);
-        return ErrorResponse.of(ErrorCode.NOT_EXIST_API);
+        log.debug("NoHandlerFoundException", e);
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(ErrorCode.NOT_EXIST_API, null));
     }
 
-    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class) // 메소드 validation 예외 상황
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
-        logWarn(e);
-        return ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
+        log.debug("IllegalArgumentException", e);
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, null));
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HttpMessageNotReadableException.class) // JSON 파싱 예외
     public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        logError(e);
-        return ErrorResponse.of(ErrorCode.INVALID_JSON);
+        log.debug("HttpMessageNotReadableException", e);
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(ErrorCode.INVALID_JSON, null));
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentTypeMismatchException.class) // 쿼리 파라미터 형식 매칭 실패 예외
     public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
-        logError(e);
-        return ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
-    }
-    @ResponseStatus(HttpStatus.REQUEST_ENTITY_TOO_LARGE)
-    @ExceptionHandler(MaxUploadSizeExceededException .class) // 파일 용량 제한 예외
-    public ResponseEntity<ErrorResponse> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
-        logError(e);
-        return ErrorResponse.of(ErrorCode.FILE_TOO_LARGE);
+        log.debug("MethodArgumentTypeMismatchException", e);
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, null));
     }
 
-    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(MaxUploadSizeExceededException.class) // 파일 용량 제한 예외
+    public ResponseEntity<ErrorResponse> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        log.debug("MaxUploadSizeExceededException", e);
+        return ResponseEntity.status(HttpStatus.REQUEST_ENTITY_TOO_LARGE) // TODO : 이거 deprecated 처리되어서 바꿔야 할듯
+                .body(ErrorResponse.of(ErrorCode.FILE_TOO_LARGE, null));
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(HttpServletRequest request, Exception e) {
-        logError(e);
-        return ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
-    }
-
-    private void logService(HttpServletRequest request, ServiceException e, ErrorCode errorCode) {
-        log.debug(EXCEPTION_REQUEST_URI, request.getRequestURI());
-        log.debug(EXCEPTION_HTTP_METHOD_TYPE, request.getMethod());
-//        log.debug(MessageUtil.getMessage(e.getMessageKey()));
-        log.warn(EXCEPTION_TYPE_FORMAT, e.getClass().getSimpleName());
-        log.warn(EXCEPTION_MESSAGE_FORMAT, e.getMessage());
-    }
-
-    private void logDebug(HttpServletRequest request, Exception e) {
-        log.debug(EXCEPTION_REQUEST_URI, request.getRequestURI());
-        log.debug(EXCEPTION_HTTP_METHOD_TYPE, request.getMethod());
-        log.debug(EXCEPTION_TYPE_FORMAT, e.getClass().getSimpleName());
-        log.debug(EXCEPTION_MESSAGE_FORMAT, e.getMessage());
-    }
-
-    private void logWarn(Exception e) {
-        log.warn(EXCEPTION_TYPE_FORMAT, e.getClass().getSimpleName());
-        log.warn(EXCEPTION_MESSAGE_FORMAT, e.getMessage());
-    }
-
-    private void logError(Exception e) {
-        log.error(EXCEPTION_TYPE_FORMAT, e.getClass().getSimpleName());
-        log.error(EXCEPTION_MESSAGE_FORMAT, e.getMessage());
-        log.error(EXCEPTION_FORMAT, e);
+        log.debug("Exception", e);
+        return ResponseEntity.internalServerError()
+                .body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, null));
     }
 }
