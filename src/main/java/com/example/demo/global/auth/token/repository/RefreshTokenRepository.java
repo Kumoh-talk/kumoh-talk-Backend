997099b85157c5397d6d901ba90aa6c9f15299c6
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-04-01 11:20:14 +0900

    [refactor] jwt 관련 파일 삭제

--- src/main/java/com/example/demo/global/auth/token/repository/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.global.auth.token.repository;
-
-import com.example.demo.global.auth.token.domain.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-}
