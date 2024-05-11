ca1c587db15e8c87ba46994d07532bd69956cd75
siwan9 <si.wan208@gmail.com>
2024-05-11 17:59:14 +0900

    [enh] 게시물 별 댓글 get 기능 추가

--- src/main/java/com/example/demo/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/demo/domain/comment/repository/CommentRepository.java
@@ -1,7 +1,14 @@
 package com.example.demo.domain.comment.repository;
 
 import com.example.demo.domain.comment.domain.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment,Long> {
+    List<Comment> findByBoard_IdOrderByParentComment_IdAsc(Long boardId);
+
 }
--- src/main/java/com/example/demo/domain/comment/service/CommentService.java
+++ src/main/java/com/example/demo/domain/comment/service/CommentService.java
@@ -4,7 +4,8 @@ package com.example.demo.domain.comment.service;
 import com.example.demo.domain.board.Repository.BoardRepository;
 import com.example.demo.domain.comment.domain.entity.Comment;
 import com.example.demo.domain.comment.domain.request.CommentRequest;
-import com.example.demo.domain.comment.domain.response.CommentInfoResponse;
+import com.example.demo.domain.comment.domain.response.CommentInfo;
+import com.example.demo.domain.comment.domain.response.CommentResponse;
 import com.example.demo.domain.comment.repository.CommentRepository;
 import com.example.demo.domain.board.domain.entity.Board;
 import com.example.demo.domain.user.domain.User;
@@ -15,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,33 +27,51 @@ public class CommentService {
     private BoardRepository boardRepository;
     private UserRepository userRepository;
 
+    @Transactional(readOnly = true)
+    public CommentResponse findByBoardId(Long boardId) {
+        boardRepository.findPostByIdWithComments(boardId)
+            .orElseThrow(() ->
+                    new ServiceException(ErrorCode.BOARD_NOT_FOUND)
+            );
+        List<Comment> comments = commentRepository.findByBoard_IdOrderByParentComment_IdAsc(boardId);
+
+        return CommentResponse.from(comments.stream()
+                .map(CommentInfo::from)
+                .collect(Collectors.toList()));
+    }
+
     @Transactional
-    public CommentInfoResponse save(Long userId, CommentRequest commentRequest,Long boardId) {
+    public CommentInfo save(Long userId, CommentRequest commentRequest, Long boardId) {
         Board findBoard = boardRepository.findById(boardId).orElseThrow(() ->
                 new ServiceException(ErrorCode.BOARD_NOT_FOUND));
         User findUser = userRepository.findById(userId)
                 .orElseThrow(() -> new ServiceException(ErrorCode.FAIL_USER_LOGIN));
 
+        Comment requestComment = new Comment(commentRequest.getContents(), findBoard, findUser, commentRequest.getDepth());
+        Comment saved = commentRepository.save(requestComment);
+
         Comment parentComment;
-        if (commentRequest.getParentId() == null)
-            parentComment = null;
+        if (commentRequest.getDepth() == 0)
+            parentComment = requestComment;
         else
-            parentComment = commentRepository.findById(commentRequest.getParentId()).orElseThrow(() ->
+            parentComment = commentRepository.findById(commentRequest.getGroupId()).orElseThrow(() ->
                     new ServiceException(ErrorCode.PARENT_NOT_FOUND));
 
-        Comment saved = commentRepository.save(new Comment(commentRequest.getContents(), findBoard, findUser, parentComment));
-        return CommentInfoResponse.of(saved, findUser.getName());
+        // 더티 체킹
+        saved.setParentComment(parentComment);
+
+        return CommentInfo.from(saved);
     }
 
     @Transactional
-    public CommentInfoResponse update(CommentRequest commentRequest,
-                                      Long commentId,
-                                      String username) {
+    public CommentInfo update(CommentRequest commentRequest,
+                              Long commentId,
+                              String username) {
         Comment findComment = commentRepository.findById(commentId).orElseThrow(() ->
                 new ServiceException(ErrorCode.COMMENT_NOT_FOUND)
         );
         findComment.changeContent(commentRequest.getContents());
-        return CommentInfoResponse.of(findComment,username);
+        return CommentInfo.from(findComment);
     }
     @Transactional
     public void delete(Long commentId) {
@@ -59,18 +79,5 @@ public class CommentService {
                 new ServiceException(ErrorCode.COMMENT_NOT_FOUND)
         );
         commentRepository.delete(findComment);
-    }
-    @Transactional(readOnly = true)
-    public List<CommentInfoResponse> findByPostId(Long postId) {
-        Board board = boardRepository.findPostByIdWithComments(postId)
-                .orElseThrow(() ->
-                        new ServiceException(ErrorCode.BOARD_NOT_FOUND)
-                );
-
-        // boar의 댓글을 Page<Comment> 객체로 형변환하여 페이징처리
-
-        return board.getComments().stream()
-                .map(comment -> Comment.newCommentInfoResponse(comment))
-                .collect(Collectors.toList());
     }
 }
