d5abab512eaee2b27a61899d91f6916b255fb88b
김건우 <96743351+kimgunwooo@users.noreply.github.com>
2024-07-12 18:06:01 +0900

    enh : 로그인 성공 시 토큰 발급 & 저장 (access, refresh)

--- src/main/java/com/example/demo/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/demo/domain/user/repository/UserRepository.java
@@ -1,16 +1,14 @@
 package com.example.demo.domain.user.repository;
 
 import com.example.demo.domain.user.domain.User;
+import com.example.demo.global.oauth.user.OAuth2Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    boolean existsByUserId(String userId);
-
-    Optional<User> findByUserId(String userId);
+    Optional<User> findByProviderAndProviderId(OAuth2Provider provider, String providerId);
 
     Optional<User> findByNickname(String nickname);
 
