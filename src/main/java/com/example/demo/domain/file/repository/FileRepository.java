97fd74f2a7621ca8bf8e60d12db19f8d93cfc39f
Kkang <aorl2313@naver.com>
2024-08-22 03:33:36 +0900

    chore : 안쓰는 File 도메인 관련 파일 삭제

--- src/main/java/com/example/demo/domain/file/repository/FileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.domain.file.repository;
-
-import com.example.demo.domain.file.domain.entity.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface FileRepository extends JpaRepository<File, Long> {
-}
