7775270b523a498ba0f55af1bccfd3ce14c27cd9
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-09-03 20:03:57 +0900

    refactor : custom enum validation 적용

--- src/main/java/com/example/demo/domain/user_addtional_info/domain/dto/request/CreateUserAdditionalInfoRequest.java
+++ src/main/java/com/example/demo/domain/user_addtional_info/domain/dto/request/CreateUserAdditionalInfoRequest.java
@@ -1,6 +1,7 @@
 package com.example.demo.domain.user_addtional_info.domain.dto.request;
 
 import com.example.demo.domain.user_addtional_info.domain.vo.StudentStatus;
+import com.example.demo.global.aop.valid.ValidEnum;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -12,7 +13,7 @@ public record CreateUserAdditionalInfoRequest(
         @NotBlank(message = "학과는 빈 값일 수 없습니다.") String department,
         @NotNull(message = "학번은 빈 값일 수 없습니다.") int studentId,
         @NotNull(message = "학년은 빈 값일 수 없습니다.") int grade,
-        @NotNull(message = "재적 상태는 빈 값일 수 없습니다.") StudentStatus studentStatus, // TODO. 커스텀 어노테이션으로 수정
+        @ValidEnum(enumClass = StudentStatus.class,message = "재학 상태는 휴학, 재학, 좋업 중 하나여야 합니다.") StudentStatus studentStatus,
         @NotBlank(message = "전화번호는 빈 값일 수 없습니다.") String phoneNumber
 ) {
 }
