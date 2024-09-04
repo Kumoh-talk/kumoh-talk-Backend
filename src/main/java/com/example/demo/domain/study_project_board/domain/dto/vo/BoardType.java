1aea1abcd1c411c54c030bd326b4692fb79fdac9
siwan9 <si.wan208@gmail.com>
2024-09-05 04:03:17 +0900

    refactor : jsonCreator 구현

--- src/main/java/com/example/demo/domain/study_project_board/domain/dto/vo/BoardType.java
+++ src/main/java/com/example/demo/domain/study_project_board/domain/dto/vo/BoardType.java
@@ -1,8 +1,20 @@
 package com.example.demo.domain.study_project_board.domain.dto.vo;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum BoardType {
     STUDY,
     PROJECT,
     SEMINAR_NOTICE,
-    SEMINAR_SUMMARY,
+    SEMINAR_SUMMARY;
+
+    @JsonCreator
+    public static BoardType fromString(String value) {
+        for (BoardType boardType : BoardType.values()) {
+            if (boardType.toString().equalsIgnoreCase(value)) {
+                return boardType;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/com/example/demo/domain/study_project_board/domain/dto/vo/QuestionType.java
+++ src/main/java/com/example/demo/domain/study_project_board/domain/dto/vo/QuestionType.java
@@ -1,6 +1,18 @@
 package com.example.demo.domain.study_project_board.domain.dto.vo;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum QuestionType {
     CHOICE,
-    DESCRIPTION
+    DESCRIPTION;
+
+    @JsonCreator
+    public static QuestionType fromString(String value) {
+        for (QuestionType questionType : QuestionType.values()) {
+            if (questionType.toString().equalsIgnoreCase(value)) {
+                return questionType;
+            }
+        }
+        return null;
+    }
 }
