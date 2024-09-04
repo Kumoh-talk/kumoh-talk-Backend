9c7babd3e2b27eadf157cd6ec0799c740dcec345
siwan9 <si.wan208@gmail.com>
2024-09-04 21:00:57 +0900

    chore : 스터디, 프로젝트 게시물 태그 종류 확장

--- src/main/java/com/example/demo/domain/study_project_board/domain/dto/vo/StudyProjectBoardTag.java
+++ src/main/java/com/example/demo/domain/study_project_board/domain/dto/vo/StudyProjectBoardTag.java
@@ -2,5 +2,8 @@ package com.example.demo.domain.study_project_board.domain.dto.vo;
 
 public enum StudyProjectBoardTag {
     FRONTEND,
-    BACKEND
+    BACKEND,
+    AI,
+    MOBILE,
+    SECURITY
 }
