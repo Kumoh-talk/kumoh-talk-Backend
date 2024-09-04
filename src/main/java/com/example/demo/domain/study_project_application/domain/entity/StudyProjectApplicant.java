9fccd01f2f2878e6071d47363e2afe1daacd69be
siwan9 <si.wan208@gmail.com>
2024-09-04 21:16:21 +0900

    chore : 스터디, 프로젝트 신청자 및 신청서 soft delete 기본 적용

--- src/main/java/com/example/demo/domain/study_project_application/domain/entity/StudyProjectApplicant.java
+++ src/main/java/com/example/demo/domain/study_project_application/domain/entity/StudyProjectApplicant.java
@@ -7,11 +7,18 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "study_project_applicants")
 @NoArgsConstructor
 @Getter
+@SQLDelete(sql = "UPDATE study_project_applicants SET deleted_at = NOW() where id=?")
+@SQLRestriction(value = "deleted_at is NULL")
 public class StudyProjectApplicant extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,9 +32,19 @@ public class StudyProjectApplicant extends BaseEntity {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @OneToMany(mappedBy = "studyProjectApplicant", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<StudyProjectApplicantAnswer> answerList = new ArrayList<>();
+
     @Builder
     public StudyProjectApplicant(StudyProjectBoard studyProjectBoard, User user) {
         this.studyProjectBoard = studyProjectBoard;
         this.user = user;
+    }
+
+    public static StudyProjectApplicant from(StudyProjectBoard studyProjectBoard, User user) {
+        return StudyProjectApplicant.builder()
+                .studyProjectBoard(studyProjectBoard)
+                .user(user)
+                .build();
     }
 }
