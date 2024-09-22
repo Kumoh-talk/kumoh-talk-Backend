d1c093979a15a012284933dcdc9dd4b9400bf5f7
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-09-23 02:53:59 +0900

    chore : event 생성자를 static 메서드로 한정

--- src/main/java/com/example/demo/domain/newsletter/event/EmailNotificationEvent.java
+++ src/main/java/com/example/demo/domain/newsletter/event/EmailNotificationEvent.java
@@ -2,12 +2,17 @@ package com.example.demo.domain.newsletter.event;
 
 import com.example.demo.domain.newsletter.strategy.EmailDeliveryStrategy;
 import com.example.demo.domain.study_project_board.domain.dto.vo.BoardType;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class EmailNotificationEvent {
     private final BoardType boardType;
     private final EmailDeliveryStrategy emailStrategy;
+
+    public static EmailNotificationEvent create(BoardType boardType, EmailDeliveryStrategy emailStrategy) {
+        return new EmailNotificationEvent(boardType, emailStrategy);
+    }
 }
