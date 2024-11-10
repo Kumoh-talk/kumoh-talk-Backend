7c6fa620b111400f9389a2cb43e58382df77b992
siwan9 <si.wan208@gmail.com>
2024-11-10 20:58:15 +0900

    refactor : 모집 신청 관련 계층 리팩토링

--- src/main/java/com/example/demo/domain/recruitment_application/controller/RecruitmentApplicationController.java
+++ src/main/java/com/example/demo/domain/recruitment_application/controller/RecruitmentApplicationController.java
@@ -27,7 +27,8 @@ public class RecruitmentApplicationController {
     private final RecruitmentApplicationService recruitmentApplicationService;
 
     /**
-     * 신청 추가
+     * [모집 신청 추가] <br>
+     * 모집 공고에 신청서 추가
      */
     @AssignUserId
     @PreAuthorize("isAuthenticated() and hasAnyRole('ROLE_ACTIVE_USER')")
@@ -40,23 +41,28 @@ public class RecruitmentApplicationController {
     }
 
     /**
-     * 게시물 별 신청 리스트 조회(applicant 테이블 get)
+     * [모집 게시물 별 신청 페이징 리스트 조회] <br>
+     * 페이지 번호로 구현된 모집 게시물 작성자가 화인할 수 있는 신청 페이징 리스트 조회
      *
-     * @param : size(페이징 사이즈), page(페이지 번호)
+     * @param pageable 페이지 번호(page), 페이지 사이즈(size), 페이지 정렬 조건 및 정렬 방향(sort) <br>
+     *                 -> 정렬 조건은 createdAt <br>
+     *                 -> 정렬 방향은 asc, desc 중 선택
      */
-    // TODO : 디자인이 확정되면 응답 포맷 확실히
     @AssignUserId
     @PreAuthorize("isAuthenticated() and hasAnyRole('ROLE_ACTIVE_USER')")
     @GetMapping("/{recruitmentBoardId}")
     public ResponseEntity<ResponseBody<RecruitmentApplicantPageResponse>> getApplicantList(
             Long userId,
-            @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            @PathVariable Long recruitmentBoardId) {
+            @PathVariable Long recruitmentBoardId,
+            @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
         return ResponseEntity.ok(createSuccessResponse(recruitmentApplicationService.getApplicantList(userId, pageable, recruitmentBoardId, false)));
     }
 
     /**
-     * applicant id를 사용하여 신청 정보 상세 조회
+     * [신청서 정보 상세 조회] <br>
+     * 신청서 목록 창에서 applicantId를 사용하여 해당 신청자의 신청서 정보를 상세 조회
+     *
+     * @apiNote applicant 테이블 도입 이유 -> recruitment_applicants_answers 테이블에 저장된 신청자들의 답변 중 동일한 신청서의 답변들을 묶기 위해
      */
     @AssignUserId
     @PreAuthorize("isAuthenticated() and hasAnyRole('ROLE_ACTIVE_USER')")
@@ -69,7 +75,8 @@ public class RecruitmentApplicationController {
     }
 
     /**
-     * 해당 신청 게시물에 신청한 내용 수정 -> 마감기한 지나면 불가능
+     * [신청서 정보 변경] <br>
+     * 마감기한 전일 경우 신청자가 신청서 정보를 변경가능
      */
     @AssignUserId
     @PreAuthorize("isAuthenticated() and hasAnyRole('ROLE_ACTIVE_USER')")
@@ -82,7 +89,8 @@ public class RecruitmentApplicationController {
     }
 
     /**
-     * 해당 신청 게시물에 신청한 내용 삭제 -> 마감기한 지나면 불가능
+     * [지정 신청서 삭제] <br>
+     * 마감기한 전일 경우 신청자가 자신의 신청서를 삭제
      */
     @AssignUserId
     @PreAuthorize("isAuthenticated() and hasAnyRole('ROLE_ACTIVE_USER')")
@@ -95,9 +103,12 @@ public class RecruitmentApplicationController {
     }
 
     /**
-     * user id를 사용하여 신청 리스트 조회(마이페이지)
+     * [사용자 신청 페이징 리스트 조회] <br>
+     * 페이지 번호로 구현된 마이페이지에 출력될 신청 페이징 리스트 조회
      *
-     * @param : size(페이징 사이즈), page(페이지 번호), boardType[study, project, mentoring]
+     * @param pageable 페이지 번호(page), 페이지 사이즈(size), 페이지 정렬 조건 및 정렬 방향(sort) <br>
+     *                 -> 정렬 조건은 createdAt <br>
+     *                 -> 정렬 방향은 asc, desc 중 선택
      */
     @AssignUserId
     @PreAuthorize("isAuthenticated() and hasAnyRole('ROLE_ACTIVE_USER')")
--- src/main/java/com/example/demo/domain/recruitment_application/repository/RecruitmentApplicantAnswerRepository.java
+++ src/main/java/com/example/demo/domain/recruitment_application/repository/RecruitmentApplicantAnswerRepository.java
@@ -5,12 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface RecruitmentApplicantAnswerRepository extends JpaRepository<RecruitmentApplicantAnswer, Long> {
 
     @Query("SELECT a FROM RecruitmentApplicantAnswer a " +
             "JOIN FETCH a.recruitmentFormQuestion " +
             "WHERE a.recruitmentApplicant.id = :applicantId")
-    Optional<List<RecruitmentApplicantAnswer>> findByRecruitmentApplicant_IdFetchQuestion(Long applicantId);
+    List<RecruitmentApplicantAnswer> findByRecruitmentApplicant_IdFetchQuestion(Long applicantId);
 }
