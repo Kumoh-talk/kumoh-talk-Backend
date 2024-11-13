3bd5dc9b908ad19ae84cc8ba37b2cc475fb362c6
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-11-13 09:14:42 +0900

    chore : 뉴스레터가 비회원도 사용할 수 있도록 수정

--- src/main/java/com/example/demo/domain/newsletter/domain/dto/request/NewsletterUpdateNotifyRequest.java
+++ src/main/java/com/example/demo/domain/newsletter/domain/dto/request/NewsletterUpdateNotifyRequest.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Pattern;
 import static com.example.demo.global.regex.UserRegex.EMAIL_REGEXP;
 
 public record NewsletterUpdateNotifyRequest(
+        @Pattern(regexp = EMAIL_REGEXP, message = "이메일 정규식을 맞춰주세요.") String email,
         @NotNull(message = "세미나 내용정리 새 글 업데이트 알림 여/부를 선택해주세요.") Boolean seminarContentNotice,
         @NotNull(message = "스터디 새 글 업데이트 알림 여/부를 선택해주세요.") Boolean studyNotice,
         @NotNull(message = "프로젝트 새 글 업데이트 알림 여/부를 선택해주세요.") Boolean projectNotice,
