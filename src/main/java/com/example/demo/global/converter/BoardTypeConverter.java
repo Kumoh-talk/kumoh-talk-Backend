7a612229b92ff1823ac77b62d0718f1f3a3464c2
siwan9 <si.wan208@gmail.com>
2025-01-14 19:43:27 +0900

    refactor : 응답 클래스 자잘한 수정 및 소문자 enum 요청 매핑 성공을 위한 수정

--- src/main/java/com/example/demo/global/converter/BoardTypeConverter.java
+++ src/main/java/com/example/demo/global/converter/BoardTypeConverter.java
@@ -1,20 +1,19 @@
 package com.example.demo.global.converter;
 
-import org.springframework.core.convert.converter.Converter;
-
 import com.example.demo.domain.board.domain.dto.vo.BoardType;
 import com.example.demo.global.base.exception.ErrorCode;
 import com.example.demo.global.base.exception.ServiceException;
+import org.springframework.core.convert.converter.Converter;
 
 public class BoardTypeConverter implements Converter<String, BoardType> {
 
-	@Override
-	public BoardType convert(String source) {
-		for (BoardType boardType : BoardType.values()) {
-			if (boardType.toString().equals(source)) {
-				return boardType;
-			}
-		}
-		throw new ServiceException(ErrorCode.BOARD_TYPE_PARSE_ERROR);
-	}
+    @Override
+    public BoardType convert(String source) {
+        for (BoardType boardType : BoardType.values()) {
+            if (boardType.toString().equalsIgnoreCase(source)) {
+                return boardType;
+            }
+        }
+        throw new ServiceException(ErrorCode.BOARD_TYPE_PARSE_ERROR);
+    }
 }
