cee2e3c4ac686e59bd114c25bf6c412af1ca6059
siwan9 <si.wan208@gmail.com>
2025-01-14 19:15:06 +0900

    refactor : Comment 테이블 분리에 따른 인터페이스, Swagger 적용 추상클래스를 통한 컨트롤러 분리

--- src/main/java/com/example/demo/domain/comment/controller/BoardCommentController.java
+++ src/main/java/com/example/demo/domain/comment/controller/BoardCommentController.java
@@ -1,18 +1,20 @@
 package com.example.demo.domain.comment.controller;
 
 
-import com.example.demo.domain.comment.controller.swagger.CommentApi;
+import com.example.demo.domain.board.domain.dto.vo.BoardType;
+import com.example.demo.domain.comment.controller.swagger.AbstractBoardCommentApi;
 import com.example.demo.domain.comment.domain.dto.request.CommentRequest;
 import com.example.demo.domain.comment.domain.dto.response.CommentInfoResponse;
-import com.example.demo.domain.comment.domain.dto.response.CommentPageResponse;
 import com.example.demo.domain.comment.domain.dto.response.CommentResponse;
-import com.example.demo.domain.comment.domain.vo.CommentTargetBoardType;
-import com.example.demo.domain.comment.service.CommentService;
+import com.example.demo.domain.comment.domain.dto.response.MyCommentResponse;
+import com.example.demo.domain.comment.service.AbstractCommentService;
+import com.example.demo.domain.notification.domain.vo.NotificationType;
 import com.example.demo.domain.recruitment_board.domain.vo.EntireBoardType;
 import com.example.demo.global.aop.AssignUserId;
 import com.example.demo.global.base.dto.ResponseBody;
+import com.example.demo.global.base.dto.page.GlobalPageResponse;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -23,68 +25,70 @@ import org.springframework.web.bind.annotation.*;
 import static com.example.demo.global.base.dto.ResponseUtil.createSuccessResponse;
 
 @RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/v1/comments")
-public class CommentController implements CommentApi {
-    private final CommentService commentService;
+@RequestMapping("/api/v1/board/comments")
+public class BoardCommentController extends AbstractBoardCommentApi {
+    private final AbstractCommentService commentService;
+
+    public BoardCommentController(
+            @Qualifier("boardCommentService") AbstractCommentService commentService) {
+        this.commentService = commentService;
+    }
 
     /**
-     * [게시물 별 댓글 조회]<br>
-     * 게시물 id에 해당하는 댓글 조회
+     * [마크다운 게시글 댓글 조회] <br>
+     * 마크다운 게시물 id에 해당하는 댓글 조회
      *
-     * @param commentTargetBoardType [basic, recruitment]
      * @apiNote 1. 삭제된 댓글도 응답으로 보낸다 <br>
      * -> 프론트에서 deletedAt 널 체크 후 "삭제되었습니다"로 내용 변경이 필요 <br>
      * 2. 기획의 요청에 따른 페이징 처리 X <br>
-     * 3. 쿼리파라미터의 CommentTargetBoardType는 대소문자 따지지않고 스펠링만 맞으면 역직렬화 성공하도록 구현
      */
+    @Override
     @GetMapping("/{boardId}")
     public ResponseEntity<ResponseBody<CommentResponse>> getBoardComments(
-            @PathVariable Long boardId,
-            @RequestParam CommentTargetBoardType commentTargetBoardType) {
+            @PathVariable Long boardId) {
         // TODO : 내가 차단한 사용자 댓글은 보이지 않도록(로그인했을 시)
-        return ResponseEntity.ok(createSuccessResponse(commentService.findCommentsByBoardId(boardId, commentTargetBoardType)));
+        return ResponseEntity.ok(createSuccessResponse(commentService.findCommentsByBoardId(boardId)));
     }
 
     /**
-     * [사용자 작성 댓글 조회] <br>
-     * 사용자 정보 창에서 게시판 타입 선택 시 타입에 해당하는 게시물에 작성했던 사용자의 댓글 조회
+     * [마크다운 게시물의 사용자 작성 댓글 조회] <br>
+     * 사용자 정보 창에서 마크다운 게시판 타입 선택 시 타입에 해당하는 게시물에 작성했던 사용자의 댓글 조회
      *
-     * @param entireBoardType [study, project, mentoring, seminar_notice, seminar_summary]
-     * @apiNote 1. QueryDsl 에 정렬 조건 넣기가 까다로워서 일단 날짜순으로 내림차순 정렬 하드코딩 해놨음 <br>
+     * @param boardType [SEMINAR, NOTICE]
+     * @apiNote 1. 날짜순으로 내림차순 정렬 하드코딩 해놨음 <br>
      * 2. 쿼리파라미터의 BoardType은 대소문자 따지지않고 스펠링만 맞으면 역직렬화 성공하도록 구현
      */
+    @Override
     @AssignUserId
     @PreAuthorize("isAuthenticated() and hasAnyRole('ROLE_USER')")
     @GetMapping("/my-comments")
-    public ResponseEntity<ResponseBody<CommentPageResponse>> getUserComments(
+    public ResponseEntity<ResponseBody<GlobalPageResponse<MyCommentResponse>>> getUserComments(
             Long userId,
             @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            @RequestParam EntireBoardType entireBoardType) {
-        return ResponseEntity.ok(createSuccessResponse(commentService.findCommentsByUserId(userId, pageable, entireBoardType)));
+            @RequestParam BoardType boardType) {
+        return ResponseEntity.ok(createSuccessResponse(commentService.findCommentsByUserId(userId, pageable, EntireBoardType.fromBoardType(boardType))));
     }
 
     /**
-     * [댓글 저장] <br>
+     * [마크다운 게시물 댓글 저장] <br>
      * 게시글에서 댓글 작성 후 저장
-     *
-     * @param commentTargetBoardType [basic, recruitment]
      */
+    @Override
     @AssignUserId
     @PreAuthorize("isAuthenticated() and hasAnyRole('ROLE_USER')")
     @PostMapping("/{boardId}")
     public ResponseEntity<ResponseBody<CommentInfoResponse>> createComment(
             Long userId,
             @PathVariable Long boardId,
-            @RequestParam CommentTargetBoardType commentTargetBoardType,
             @RequestBody @Valid CommentRequest commentRequest) {
-        return ResponseEntity.ok(createSuccessResponse(commentService.saveComment(userId, boardId, commentTargetBoardType, commentRequest)));
+        return ResponseEntity.ok(createSuccessResponse(commentService.saveComment(userId, boardId, commentRequest, NotificationType.BOARD_COMMENT)));
     }
 
     /**
-     * [댓글 수정] <br>
+     * [마크다운 게시물 댓글 수정] <br>
      * 작성한 댓글 수정
      */
+    @Override
     @AssignUserId
     @PreAuthorize("isAuthenticated() and hasAnyRole('ROLE_USER')")
     @PatchMapping("/{commentId}")
@@ -96,17 +100,18 @@ public class CommentController implements CommentApi {
     }
 
     /**
-     * [댓글 삭제] <br>
+     * [마크다운 게시물 댓글 삭제] <br>
      * 작성한 댓글 soft 삭제
      *
      * @apiNote 1. 삭제된 댓글도 응답으로 보내야하므로 Comment 엔티티에 SQLRestriction 처리를 해놓지 않았음 <br>
      * 2. 관리자 기능과 달리 로그인한 유저의 id와 댓글 작성 유저의 id를 비교하는 절차를 거쳐야하므로, isAuthorized 매개변수를 false로 설정함
      */
+    @Override
     @AssignUserId
     @PreAuthorize("isAuthenticated() and hasAnyRole('ROLE_USER')")
     @DeleteMapping("/{commentId}")
     public ResponseEntity<ResponseBody<Void>> deleteComment(Long userId, @PathVariable Long commentId) {
-        commentService.deleteComment(userId, commentId, false);
+        commentService.deleteComment(userId, commentId, false, NotificationType.BOARD_COMMENT);
         return ResponseEntity.ok().body(createSuccessResponse());
     }
 }
