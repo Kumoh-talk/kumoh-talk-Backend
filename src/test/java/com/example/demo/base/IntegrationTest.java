89b1cf92bcc44ef88167573f82228f82f7b06ec3
Kkang <aorl2313@naver.com>
2025-01-21 02:19:19 +0900

    test: test Fixtures 개발 패턴 적용 및 세미나/공지사항 게시물 상세조회 통합테스트 작성

---
+++ src/test/java/com/example/demo/base/IntegrationTest.java
@@ -0,0 +1,41 @@
+package com.example.demo.base;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.event.ApplicationEvents;
+import org.springframework.test.context.event.RecordApplicationEvents;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.base.config.RedisContainerConfig;
+import com.example.demo.builder.BuilderSupporter;
+import com.example.demo.builder.TestFixtureBuilder;
+import com.example.demo.domain.board.service.usecase.BoardUseCase;
+
+@SpringJUnitConfig // Spring 테스트 컨텍스트 활성화
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+@Import(value = {TestFixtureBuilder.class, BuilderSupporter.class, RedisContainerConfig.class})
+@RecordApplicationEvents // 애플리케이션 이벤트 기록 활성화
+public abstract class IntegrationTest {
+
+
+	@Autowired
+	protected TestFixtureBuilder testFixtureBuilder;
+
+	@Autowired
+	protected RedisTemplate<String, Object> redisTemplate;
+
+	@Autowired
+	protected ApplicationEvents events;
+
+	@Autowired
+	protected BoardUseCase boardUseCase;
+
+}
--- src/test/java/com/example/demo/domain/board/controller/BoardControllerIntegrationTest.java
+++ src/test/java/com/example/demo/domain/board/controller/BoardControllerIntegrationTest.java
@@ -1,24 +1,147 @@
 package com.example.demo.domain.board.controller;
 
 
-import static org.junit.jupiter.api.Assertions.*;
 
+
+import static com.example.demo.fixture.board.BoardFixtures.*;
+import static com.example.demo.fixture.user.UserFixtures.*;
+import static org.assertj.core.api.SoftAssertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.security.core.context.SecurityContextHolder;
 
-@SpringBootTest
-@ActiveProfiles("test")
-public class BoardControllerIntegrationTest {
+import com.example.demo.base.IntegrationTest;
+import com.example.demo.domain.board.domain.dto.response.BoardInfoResponse;
+import com.example.demo.domain.board.domain.entity.Board;
+import com.example.demo.domain.user.domain.User;
+import com.example.demo.global.base.exception.ErrorCode;
+import com.example.demo.global.base.exception.ServiceException;
+import com.example.demo.global.event.view.BoardViewEvent;
+import com.example.demo.global.jwt.JwtAuthentication;
+import com.example.demo.global.jwt.JwtUserClaim;
 
-	@Autowired
-	private Environment environment;
+public class BoardControllerIntegrationTest extends IntegrationTest {
 
-	@Test
-	public void testActiveProfile() {
-		String[] activeProfiles = environment.getActiveProfiles();
-		assertEquals("test", activeProfiles[0]);
+
+	@Nested
+	@DisplayName("<세미나/공지사항 게시물 상세 조회>")
+	class Describe_getSeminarNoticeBoardDetail {
+		private User savedUser;
+
+		private static final String BOARD_VIEW_KEY = "board:view:";
+
+		@BeforeEach
+		void setUp() {
+			savedUser = testFixtureBuilder.buildUser(ADMIN_USER());
+		}
+
+		@AfterEach
+		void tearDown() {
+			redisTemplate.opsForList().getOperations().delete("board:view:");
+		}
+
+		@Test
+		void 성공_조휘수캐싱_ApplicationEvent_데이터를_조회할수있다() {
+			// given
+			Board publishedBOARD = testFixtureBuilder.buildBoard(PUBLISHED_SEMINAR_BOARD(savedUser));
+
+			// when
+			BoardInfoResponse boardInfoResponse = boardUseCase.searchSingleBoard(publishedBOARD.getId());
+
+			// then
+			assertSoftly(softly -> {
+				softly.assertThat(boardInfoResponse.getBoardId()).isEqualTo(publishedBOARD.getId());
+				softly.assertThat(boardInfoResponse.getTitle()).isEqualTo(publishedBOARD.getTitle());
+				softly.assertThat(boardInfoResponse.getContents()).isEqualTo(publishedBOARD.getContent());
+				softly.assertThat(boardInfoResponse.getBoardType()).isEqualTo(publishedBOARD.getBoardType().toString());
+				softly.assertThat(boardInfoResponse.getStatus()).isEqualTo(publishedBOARD.getStatus().toString());
+				softly.assertThat(boardInfoResponse.getView()).isEqualTo(publishedBOARD.getViewCount());
+				softly.assertThat(boardInfoResponse.getLike()).isEqualTo(publishedBOARD.getLikes().size());
+				softly.assertThat(boardInfoResponse.getCreatedAt()).isEqualTo(publishedBOARD.getCreatedAt());
+				softly.assertThat(boardInfoResponse.getUpdatedAt()).isEqualTo(publishedBOARD.getUpdatedAt());
+			});
+
+			assertSoftly(softly -> {
+				softly.assertThat(redisTemplate.opsForValue().get(BOARD_VIEW_KEY + publishedBOARD.getId()))
+					.isEqualTo("1");
+				softly.assertThat(events.stream(BoardViewEvent.class).count()).isEqualTo(1);
+			});
+
+		}
+
+		@Test
+		void 성공_임시저장_게시물은_게시물_소유자만_조회할_수_있다() {
+			// given
+			Board draftBoard = testFixtureBuilder.buildBoard(DRAFT_SEMINAR_BOARD(savedUser));
+			SecurityContextHolder.getContext().setAuthentication(new JwtAuthentication(JwtUserClaim.create(savedUser)));
+
+			// when
+			BoardInfoResponse boardInfoResponse = boardUseCase.searchSingleBoard(draftBoard.getId());
+
+			// then
+			assertSoftly(softly -> {
+				softly.assertThat(boardInfoResponse.getBoardId()).isEqualTo(draftBoard.getId());
+				softly.assertThat(boardInfoResponse.getTitle()).isEqualTo(draftBoard.getTitle());
+				softly.assertThat(boardInfoResponse.getContents()).isEqualTo(draftBoard.getContent());
+				softly.assertThat(boardInfoResponse.getBoardType()).isEqualTo(draftBoard.getBoardType().toString());
+				softly.assertThat(boardInfoResponse.getStatus()).isEqualTo(draftBoard.getStatus().toString());
+				softly.assertThat(boardInfoResponse.getView()).isEqualTo(draftBoard.getViewCount());
+				softly.assertThat(boardInfoResponse.getLike()).isEqualTo(draftBoard.getLikes().size());
+				softly.assertThat(boardInfoResponse.getCreatedAt()).isEqualTo(draftBoard.getCreatedAt());
+				softly.assertThat(boardInfoResponse.getUpdatedAt()).isEqualTo(draftBoard.getUpdatedAt());
+			});
+
+			assertSoftly(softly -> {
+				softly.assertThat(redisTemplate.opsForValue().get( BOARD_VIEW_KEY+ draftBoard.getId()))
+					.isEqualTo("1");
+				softly.assertThat(events.stream(BoardViewEvent.class).count()).isEqualTo(1);
+			});
+		}
+
+		@Test
+		void 실패_임시저장_게시물은_게시물_소유자만_조회할_수_있다() {
+			// given
+			Board draftBoard = testFixtureBuilder.buildBoard(DRAFT_SEMINAR_BOARD(savedUser));
+
+
+
+
+			SecurityContextHolder.getContext()
+				.setAuthentication(
+					new JwtAuthentication(JwtUserClaim.create(testFixtureBuilder.buildUser(SEMINAR_WRITER_USER()))));
+
+
+			// when -> then
+			assertSoftly(softly -> {
+				softly.assertThatThrownBy(() -> boardUseCase.searchSingleBoard(draftBoard.getId()))
+					.isInstanceOf(ServiceException.class)
+					.hasFieldOrPropertyWithValue("errorCode", ErrorCode.DRAFT_NOT_ACCESS_USER);
+				softly.assertThat(redisTemplate.opsForValue().get(BOARD_VIEW_KEY + draftBoard.getId()))
+					.isNull();
+				softly.assertThat(events.stream(BoardViewEvent.class).count()).isEqualTo(0);
+			});
+		}
+
+		@Test
+		void 실패_유효하지_않는_게시물_조회() {
+			// given
+			Board draftBoard = testFixtureBuilder.buildBoard(DRAFT_SEMINAR_BOARD(savedUser));
+
+			// when -> then
+			assertSoftly(softly -> {
+				softly.assertThatThrownBy(() -> boardUseCase.searchSingleBoard(draftBoard.getId() + 1))
+					.isInstanceOf(ServiceException.class)
+					.hasFieldOrPropertyWithValue("errorCode", ErrorCode.BOARD_NOT_FOUND);
+				softly.assertThat(redisTemplate.opsForValue().get(BOARD_VIEW_KEY + draftBoard.getId()))
+					.isNull();
+				softly.assertThat(events.stream(BoardViewEvent.class).count()).isEqualTo(0);
+			});
+
+		}
 	}
+
 }
