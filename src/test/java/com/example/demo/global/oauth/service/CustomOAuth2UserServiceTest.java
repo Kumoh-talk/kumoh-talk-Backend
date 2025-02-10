c559007235d8a54f488e7e06c7bb3d05e0bf453f
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2025-02-10 16:50:24 +0900

    test: 접근 지정자 추가 및 테스트에 필요한 상수를 변수화

--- src/test/java/com/example/demo/global/oauth/service/CustomOAuth2UserServiceTest.java
+++ src/test/java/com/example/demo/global/oauth/service/CustomOAuth2UserServiceTest.java
@@ -70,60 +70,60 @@ public class CustomOAuth2UserServiceTest {
     @Nested
     @DisplayName("GitHub 사용자 정보 획득")
     class GetGithubLoginUserInformation {
-        String endpointUri = "https://api.github.com/user";
+        private String ENDPOINT_URI = "https://api.github.com/user";
 
         // 모의 OAuth 서버의 클라이언트 정보 및 발급하는 액세스 토큰 설정
-        ClientRegistration clientRegistration = createGithubClientRegistration();
-        OAuth2AccessToken accessToken = createAccessToken();
+        private ClientRegistration clientRegistration = createGithubClientRegistration();
+        private OAuth2AccessToken accessToken = createAccessToken();
         // 모의 OAuth 서버에 보낼 요청 설정
-        OAuth2UserRequest userRequest = new OAuth2UserRequest(clientRegistration, accessToken);
+        private OAuth2UserRequest userRequest = new OAuth2UserRequest(clientRegistration, accessToken);
+
+        private final long USER_INFO_ID = 123456789L;
+        private final String MOCK_RESPONSE = String.format("{"
+                + "\"login\": \"octocat\","
+                + "\"id\": %d,"
+                + "\"node_id\": \"MDQ6VXNlcjE=\",\n"
+                + "\"avatar_url\": \"https://github.com/images/error/octocat_happy.gif\","
+                + "\"gravatar_id\": \"\","
+                + "\"url\": \"https://api.github.com/users/octocat\","
+                + "\"html_url\": \"https://github.com/octocat\","
+                + "\"followers_url\": \"https://api.github.com/users/octocat/followers\","
+                + "\"following_url\": \"https://api.github.com/users/octocat/following{/other_user}\","
+                + "\"gists_url\": \"https://api.github.com/users/octocat/gists{/gist_id}\","
+                + "\"starred_url\": \"https://api.github.com/users/octocat/starred{/owner}{/repo}\","
+                + "\"subscriptions_url\": \"https://api.github.com/users/octocat/subscriptions\","
+                + "\"organizations_url\": \"https://api.github.com/users/octocat/orgs\","
+                + "\"repos_url\": \"https://api.github.com/users/octocat/repos\","
+                + "\"events_url\": \"https://api.github.com/users/octocat/events{/privacy}\","
+                + "\"received_events_url\": \"https://api.github.com/users/octocat/received_events\","
+                + "\"type\": \"User\","
+                + "\"site_admin\": false,"
+                + "\"name\": \"monalisa octocat\","
+                + "\"company\": \"GitHub\","
+                + "\"blog\": \"https://github.com/blog\","
+                + "\"location\": \"San Francisco\","
+                + "\"email\": \"octocat@github.com\","
+                + "\"hireable\": false,"
+                + "\"bio\": \"There once was...\","
+                + "\"twitter_username\": \"monatheoctocat\","
+                + "\"public_repos\": 2,"
+                + "\"public_gists\": 1,"
+                + "\"followers\": 20,"
+                + "\"following\": 0,"
+                + "\"created_at\": \"2008-01-14T04:33:35Z\","
+                + "\"updated_at\": \"2008-01-14T04:33:35Z\""
+                + "}", USER_INFO_ID);
 
         @Test
         void 성공_사용자_정보를_얻을_수_있다() {
             // given
-            // 모의 응답 설정
-            String mockResponse = "{"
-                    + "\"login\": \"octocat\","
-                    + "\"id\": 123456789,"
-                    + "\"node_id\": \"MDQ6VXNlcjE=\",\n"
-                    + "\"avatar_url\": \"https://github.com/images/error/octocat_happy.gif\"," +
-                    "\"gravatar_id\": \"\","
-                    + "\"url\": \"https://api.github.com/users/octocat\","
-                    + "\"html_url\": \"https://github.com/octocat\"," +
-                    "\"followers_url\": \"https://api.github.com/users/octocat/followers\","
-                    + "\"following_url\": \"https://api.github.com/users/octocat/following{/other_user}\","
-                    + "\"gists_url\": \"https://api.github.com/users/octocat/gists{/gist_id}\","
-                    + "\"starred_url\": \"https://api.github.com/users/octocat/starred{/owner}{/repo}\","
-                    + "\"subscriptions_url\": \"https://api.github.com/users/octocat/subscriptions\","
-                    + "\"organizations_url\": \"https://api.github.com/users/octocat/orgs\","
-                    + "\"repos_url\": \"https://api.github.com/users/octocat/repos\","
-                    + "\"events_url\": \"https://api.github.com/users/octocat/events{/privacy}\","
-                    + "\"received_events_url\": \"https://api.github.com/users/octocat/received_events\","
-                    + "\"type\": \"User\","
-                    + "\"site_admin\": false,"
-                    + "\"name\": \"monalisa octocat\","
-                    + "\"company\": \"GitHub\","
-                    + "\"blog\": \"https://github.com/blog\","
-                    + "\"location\": \"San Francisco\","
-                    + "\"email\": \"octocat@github.com\","
-                    + "\"hireable\": false,"
-                    + "\"bio\": \"There once was...\","
-                    + "\"twitter_username\": \"monatheoctocat\","
-                    + "\"public_repos\": 2,"
-                    + "\"public_gists\": 1,"
-                    + "\"followers\": 20,"
-                    + "\"following\": 0,"
-                    + "\"created_at\": \"2008-01-14T04:33:35Z\","
-                    + "\"updated_at\": \"2008-01-14T04:33:35Z\""
-                    + "}";
-
             // 모의 OAuth 서버 설정
             mockServer
                     // 요청의 HTTP 메서드 및 URL 지정
-                    .expect(requestTo(endpointUri))
+                    .expect(requestTo(ENDPOINT_URI))
                     .andExpect(method(HttpMethod.GET))
                     // 해당 HTTP 메서드로 해당 URL에 요청이 오면 HTTP 상태 코드 200과 함께 모의 응답을 반환
-                    .andRespond(withSuccess(mockResponse, MediaType.APPLICATION_JSON));
+                    .andRespond(withSuccess(MOCK_RESPONSE, MediaType.APPLICATION_JSON));
 
             // when
             OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
@@ -136,7 +136,7 @@ public class CustomOAuth2UserServiceTest {
                 softly.assertThat(userInfo).isInstanceOf(OAuth2UserInfo.class);
 
                 softly.assertThat(userInfo.getProvider()).isEqualTo(OAuth2Provider.GITHUB);
-                softly.assertThat(userInfo.getId()).isEqualTo("123456789");
+                softly.assertThat(userInfo.getId()).isEqualTo(String.valueOf(USER_INFO_ID));
 
                 softly.assertThat(userInfo.getAccessToken()).isEqualTo(createAccessToken().getTokenValue());
             });
@@ -152,7 +152,7 @@ public class CustomOAuth2UserServiceTest {
 
             // 모의 OAuth 서버 설정
             mockServer
-                    .expect(requestTo(endpointUri))
+                    .expect(requestTo(ENDPOINT_URI))
                     .andExpect(method(HttpMethod.GET))
                     .andRespond(respond);
 
@@ -183,35 +183,35 @@ public class CustomOAuth2UserServiceTest {
     @Nested
     @DisplayName("Google 사용자 정보 획득")
     class GetGoogleLoginUserInformation {
-        String endpointUri = "https://www.googleapis.com/oauth2/v3/userinfo";
+        private final String ENDPOINT_URI = "https://www.googleapis.com/oauth2/v3/userinfo";
 
         // 모의 OAuth 서버의 클라이언트 정보 및 발급하는 액세스 토큰 설정
-        ClientRegistration clientRegistration = createGoogleClientRegistration();
-        OAuth2AccessToken accessToken = createAccessToken();
+        private ClientRegistration clientRegistration = createGoogleClientRegistration();
+        private OAuth2AccessToken accessToken = createAccessToken();
         // 모의 OAuth 서버에 보낼 요청 설정
-        OAuth2UserRequest userRequest = new OAuth2UserRequest(clientRegistration, accessToken);
+        private OAuth2UserRequest userRequest = new OAuth2UserRequest(clientRegistration, accessToken);
+
+        private final String USER_INFO_ID = "123456789012345678901";
+        private final String MOCK_RESPONSE = String.format("{"
+                + "\"sub\": \"%s\","
+                + "\"name\": \"Kim Kumoh\","
+                + "\"given_name\": \"Kumoh\","
+                + "\"family_name\": \"Kim\","
+                + "\"picture\": \"https://lh3.googleusercontent.com/a/profile_image\","
+                + "\"email\": \"example@gmail.com\","
+                + "\"email_verified\": true"
+                + "}", USER_INFO_ID);
 
         @Test
         void 성공_사용자_정보를_얻을_수_있다() {
             // given
-            // 모의 응답 설정
-            String mockResponse = "{"
-                    + "\"sub\": \"123456789012345678901\","
-                    + "\"name\": \"Kim Kumoh\","
-                    + "\"given_name\": \"Kumoh\","
-                    + "\"family_name\": \"Kim\","
-                    + "\"picture\": \"https://lh3.googleusercontent.com/a/profile_image\","
-                    + "\"email\": \"example@gmail.com\","
-                    + "\"email_verified\": true"
-                    + "}";
-
             // 모의 OAuth 서버 설정
             mockServer
                     // 요청의 HTTP 메서드 및 URL 지정
-                    .expect(requestTo(endpointUri))
+                    .expect(requestTo(ENDPOINT_URI))
                     .andExpect(method(HttpMethod.GET))
                     // 해당 HTTP 메서드로 해당 URL에 요청이 오면 HTTP 상태 코드 200과 함께 모의 응답을 반환
-                    .andRespond(withSuccess(mockResponse, MediaType.APPLICATION_JSON));
+                    .andRespond(withSuccess(MOCK_RESPONSE, MediaType.APPLICATION_JSON));
 
             // when
             OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
@@ -224,7 +224,7 @@ public class CustomOAuth2UserServiceTest {
                 softly.assertThat(userInfo).isInstanceOf(OAuth2UserInfo.class);
 
                 softly.assertThat(userInfo.getProvider()).isEqualTo(OAuth2Provider.GOOGLE);
-                softly.assertThat(userInfo.getId()).isEqualTo("123456789012345678901");
+                softly.assertThat(userInfo.getId()).isEqualTo(USER_INFO_ID);
 
                 softly.assertThat(userInfo.getAccessToken()).isEqualTo(createAccessToken().getTokenValue());
             });
@@ -240,7 +240,7 @@ public class CustomOAuth2UserServiceTest {
 
             // 모의 OAuth 서버 설정
             mockServer
-                    .expect(requestTo(endpointUri))
+                    .expect(requestTo(ENDPOINT_URI))
                     .andExpect(method(HttpMethod.GET))
                     .andRespond(respond);
 
@@ -272,47 +272,47 @@ public class CustomOAuth2UserServiceTest {
     @Nested
     @DisplayName("kakao 사용자 정보 획득")
     class GetKakaoLoginUserInformation {
-        String endpointUri = "https://kapi.kakao.com/v2/user/me";
+        private final String ENDPOINT_URI = "https://kapi.kakao.com/v2/user/me";
 
         // 모의 OAuth 서버의 클라이언트 정보 및 발급하는 액세스 토큰 설정
-        ClientRegistration clientRegistration = createKakaoClientRegistration();
-        OAuth2AccessToken accessToken = createAccessToken();
+        private ClientRegistration clientRegistration = createKakaoClientRegistration();
+        private OAuth2AccessToken accessToken = createAccessToken();
         // 모의 OAuth 서버에 보낼 요청 설정
-        OAuth2UserRequest userRequest = new OAuth2UserRequest(clientRegistration, accessToken);
+        private OAuth2UserRequest userRequest = new OAuth2UserRequest(clientRegistration, accessToken);
+
+        private final long USER_INFO_ID = 9876543210L;
+        private final String MOCK_RESPONSE = String.format("{"
+                + "\"id\": %d,"
+                + "\"connected_at\": \"2022-04-11T01:45:28Z\","
+                + "\"kakao_account\": {"
+                + "     \"profile_nickname_needs_agreement\": false,"
+                + "     \"profile_image_needs_agreement\": false,"
+                + "     \"profile\": {"
+                + "         \"nickname\": \"테크모\","
+                + "         \"thumbnail_image_url\": \"http://k.kakaocdn.net/dn/thumbnail.jpg\","
+                + "         \"profile_image_url\": \"http://k.kakaocdn.net/dn/profile.jpg\","
+                + "         \"is_default_image\": false,"
+                + "         \"is_default_nickname\": false"
+                + "     },"
+                + "     \"name_needs_agreement\": false,"
+                + "     \"name\": \"김금오\","
+                + "     \"email_needs_agreement\": false,"
+                + "     \"is_email_valid\": true,"
+                + "     \"is_email_verified\": true,"
+                + "     \"email\": \"test@example.com\""
+                + "}"
+                + "}", USER_INFO_ID);
 
         @Test
         void 성공_사용자_정보를_얻을_수_있다() {
             // given
-            // 모의 응답 설정
-            String mockResponse = "{"
-                    + "\"id\": 9876543210,"
-                    + "\"connected_at\": \"2022-04-11T01:45:28Z\","
-                    + "\"kakao_account\": {"
-                    + "     \"profile_nickname_needs_agreement\": false,"
-                    + "     \"profile_image_needs_agreement\": false,"
-                    + "     \"profile\": {"
-                    + "         \"nickname\": \"테크모\","
-                    + "         \"thumbnail_image_url\": \"http://k.kakaocdn.net/dn/thumbnail.jpg\","
-                    + "         \"profile_image_url\": \"http://k.kakaocdn.net/dn/profile.jpg\","
-                    + "         \"is_default_image\": false,"
-                    + "         \"is_default_nickname\": false"
-                    + "     },"
-                    + "     \"name_needs_agreement\": false,"
-                    + "     \"name\": \"김금오\","
-                    + "     \"email_needs_agreement\": false,"
-                    + "     \"is_email_valid\": true,"
-                    + "     \"is_email_verified\": true,"
-                    + "     \"email\": \"test@example.com\""
-                    + "}"
-                    + "}";
-
             // 모의 OAuth 서버 설정
             mockServer
                     // 요청의 HTTP 메서드 및 URL 지정
-                    .expect(requestTo(endpointUri))
+                    .expect(requestTo(ENDPOINT_URI))
                     .andExpect(method(HttpMethod.GET))
                     // 해당 HTTP 메서드로 해당 URL에 요청이 오면 HTTP 상태 코드 200과 함께 모의 응답을 반환
-                    .andRespond(withSuccess(mockResponse, MediaType.APPLICATION_JSON));
+                    .andRespond(withSuccess(MOCK_RESPONSE, MediaType.APPLICATION_JSON));
 
             // when
             OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
@@ -325,7 +325,7 @@ public class CustomOAuth2UserServiceTest {
                 softly.assertThat(userInfo).isInstanceOf(OAuth2UserInfo.class);
 
                 softly.assertThat(userInfo.getProvider()).isEqualTo(OAuth2Provider.KAKAO);
-                softly.assertThat(userInfo.getId()).isEqualTo("9876543210");
+                softly.assertThat(userInfo.getId()).isEqualTo(String.valueOf(USER_INFO_ID));
 
                 softly.assertThat(userInfo.getAccessToken()).isEqualTo(createAccessToken().getTokenValue());
             });
@@ -341,7 +341,7 @@ public class CustomOAuth2UserServiceTest {
 
             // 모의 OAuth 서버 설정
             mockServer
-                    .expect(requestTo(endpointUri))
+                    .expect(requestTo(ENDPOINT_URI))
                     .andExpect(method(HttpMethod.GET))
                     .andRespond(respond);
 
@@ -381,42 +381,42 @@ public class CustomOAuth2UserServiceTest {
     @Nested
     @DisplayName("NAVER 사용자 정보 획득")
     class GetNaverLoginUserInformation {
-        String endpointUri = "https://openapi.naver.com/v1/nid/me";
+        private final String ENDPOINT_URI = "https://openapi.naver.com/v1/nid/me";
 
         // 모의 OAuth 서버의 클라이언트 정보 및 발급하는 액세스 토큰 설정
-        ClientRegistration clientRegistration = createNaverClientRegistration();
-        OAuth2AccessToken accessToken = createAccessToken();
+        private ClientRegistration clientRegistration = createNaverClientRegistration();
+        private OAuth2AccessToken accessToken = createAccessToken();
         // 모의 OAuth 서버에 보낼 요청 설정
-        OAuth2UserRequest userRequest = new OAuth2UserRequest(clientRegistration, accessToken);
+        private OAuth2UserRequest userRequest = new OAuth2UserRequest(clientRegistration, accessToken);
+
+        private final String USER_INFO_ID = "12345678";
+        private final String MOCK_RESPONSE = String.format("{"
+                + "  \"resultcode\": \"00\","
+                + "  \"message\": \"success\","
+                + "  \"response\": {"
+                + "    \"email\": \"test@example.com\","
+                + "    \"nickname\": \"테크모\","
+                + "    \"profile_image\": \"https://ssl.pstatic.net/static/pwe/address/nodata_33x33.gif\","
+                + "    \"age\": \"40-49\","
+                + "    \"gender\": \"F\","
+                + "    \"id\": \"%s\","
+                + "    \"name\": \"김금오\","
+                + "    \"birthday\": \"10-01\","
+                + "    \"birthyear\": \"1900\","
+                + "    \"mobile\": \"010-0000-0000\""
+                + "  }"
+                + "}", USER_INFO_ID);
 
         @Test
         void 성공_사용자_정보를_얻을_수_있다() {
             // given
-            // 모의 응답 설정
-            String mockResponse = "{"
-                    + "  \"resultcode\": \"00\","
-                    + "  \"message\": \"success\","
-                    + "  \"response\": {"
-                    + "    \"email\": \"test@example.com\","
-                    + "    \"nickname\": \"테크모\","
-                    + "    \"profile_image\": \"https://ssl.pstatic.net/static/pwe/address/nodata_33x33.gif\","
-                    + "    \"age\": \"40-49\","
-                    + "    \"gender\": \"F\","
-                    + "    \"id\": \"12345678\","
-                    + "    \"name\": \"김금오\","
-                    + "    \"birthday\": \"10-01\","
-                    + "    \"birthyear\": \"1900\","
-                    + "    \"mobile\": \"010-0000-0000\""
-                    + "  }"
-                    + "}";
-
             // 모의 OAuth 서버 설정
             mockServer
                     // 요청의 HTTP 메서드 및 URL 지정
-                    .expect(requestTo(endpointUri))
+                    .expect(requestTo(ENDPOINT_URI))
                     .andExpect(method(HttpMethod.GET))
                     // 해당 HTTP 메서드로 해당 URL에 요청이 오면 HTTP 상태 코드 200과 함께 모의 응답을 반환
-                    .andRespond(withSuccess(mockResponse, MediaType.APPLICATION_JSON));
+                    .andRespond(withSuccess(MOCK_RESPONSE, MediaType.APPLICATION_JSON));
 
             // when
             OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
@@ -429,7 +429,7 @@ public class CustomOAuth2UserServiceTest {
                 softly.assertThat(userInfo).isInstanceOf(OAuth2UserInfo.class);
 
                 softly.assertThat(userInfo.getProvider()).isEqualTo(OAuth2Provider.NAVER);
-                softly.assertThat(userInfo.getId()).isEqualTo("12345678");
+                softly.assertThat(userInfo.getId()).isEqualTo(USER_INFO_ID);
 
                 softly.assertThat(userInfo.getAccessToken()).isEqualTo(createAccessToken().getTokenValue());
             });
@@ -445,7 +445,7 @@ public class CustomOAuth2UserServiceTest {
 
             // 모의 OAuth 서버 설정
             mockServer
-                    .expect(requestTo(endpointUri))
+                    .expect(requestTo(ENDPOINT_URI))
                     .andExpect(method(HttpMethod.GET))
                     .andRespond(respond);
 
