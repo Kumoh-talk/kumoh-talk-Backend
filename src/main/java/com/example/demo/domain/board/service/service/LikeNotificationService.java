8e65d8bdee6c93081a152de13cc6c9d9b0c3d5a6
siwan9 <si.wan208@gmail.com>
2025-02-18 20:47:25 +0900

    test : 좋아요, 댓글 생성, 삭제에 따른 알림 엔티티 테스트 구

--- src/main/java/com/example/demo/domain/board/service/service/LikeNotificationService.java
+++ src/main/java/com/example/demo/domain/board/service/service/LikeNotificationService.java
@@ -5,29 +5,27 @@ import com.example.demo.domain.notification.domain.entity.Notification;
 import com.example.demo.domain.notification.domain.entity.NotificationUser;
 import com.example.demo.domain.notification.domain.vo.NotificationType;
 import com.example.demo.domain.notification.repository.NotificationRepository;
-import com.example.demo.domain.notification.repository.NotificationUserRepository;
-import com.example.demo.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.concurrent.CompletableFuture;
+
 @Service
 @RequiredArgsConstructor
 public class LikeNotificationService {
     private final NotificationRepository notificationRepository;
-    private final NotificationUserRepository notificationUserRepository;
 
     @Async
     @Transactional
-    public void saveLikeNotification(Like like) {
+    public CompletableFuture<Notification> saveLikeNotification(Like like) {
         Notification notification = Notification.fromLikeEntity(like, NotificationType.BOARD_LIKE);
         if (!like.getBoard().getUser().getId().equals(like.getUser().getId())) {
-            User user = like.getBoard().getUser();
-            Notification saved = notificationRepository.save(notification);
-            notificationUserRepository.save(NotificationUser.from(saved, user));
+            notification.getNotificationUserList().add(NotificationUser.from(notification, like.getBoard().getUser()));
+            return CompletableFuture.completedFuture(notificationRepository.save(notification));
         }
-
+        return CompletableFuture.completedFuture(null);
     }
 
     @Async
