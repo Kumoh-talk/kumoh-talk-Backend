059bd834601e95d582f2d9b714af32322781baf0
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2025-02-28 10:31:30 +0900

    refactor: 발행 및 구독 책임을 따로 가지도록 수정

--- src/main/java/com/example/demo/domain/newsletter/event/NewsletterEventPublisher.java
+++ src/main/java/com/example/demo/domain/newsletter/event/NewsletterEventPublisher.java
@@ -1,29 +1,15 @@
 package com.example.demo.domain.newsletter.event;
 
-import com.example.demo.domain.newsletter.repository.NewsletterRepository;
-import com.example.demo.domain.newsletter.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
-
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class NewsletterEventPublisher {
-    private final NewsletterRepository newsletterRepository;
-    private final EmailService emailService;
     private final ApplicationEventPublisher applicationEventPublisher;
 
     public void publish(EmailNotificationEvent event) {
         applicationEventPublisher.publishEvent(event);
-    }
-
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void handlePostCreatedEvent(EmailNotificationEvent event) {
-        List<String> subscriberEmails = newsletterRepository.findSubscriberEmails(String.valueOf(event.getEntireBoardType()));
-        emailService.sendEmailNotice(subscriberEmails, event.getEmailStrategy());
     }
 }
