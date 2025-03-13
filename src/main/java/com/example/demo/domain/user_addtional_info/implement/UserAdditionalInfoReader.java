b528524b1d29e44653a9910ddd7331c971b34b84
kon28289 <kon28289@naver.com>
2025-03-13 18:04:04 +0900

    refactor: user_addtional_info 리팩터링 (domain 계층)

---
+++ src/main/java/com/example/demo/domain/user_addtional_info/implement/UserAdditionalInfoReader.java
@@ -0,0 +1,30 @@
+package com.example.demo.domain.user_addtional_info.implement;
+
+import com.example.demo.domain.user_addtional_info.entity.UserAdditionalInfoData;
+import com.example.demo.domain.user_addtional_info.repository.UserAdditionalInfoRepository;
+import com.example.demo.global.base.exception.ServiceException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import static com.example.demo.global.base.exception.ErrorCode.USER_ADDITIONAL_INFO_NOT_FOUND;
+
+@Component
+@RequiredArgsConstructor
+public class UserAdditionalInfoReader {
+    private final UserAdditionalInfoRepository userAdditionalInfoRepository;
+
+    public Page<UserAdditionalInfoData> findAllUserAdditionalInfos(Pageable pageable) {
+        return userAdditionalInfoRepository.findAll(pageable);
+    }
+
+    public void checkValidateUserAdditionalInfo(UserAdditionalInfoData userAdditionalInfoData) {
+        if (userAdditionalInfoData == null) {
+            throw new ServiceException(USER_ADDITIONAL_INFO_NOT_FOUND);
+        }
+    }
+    public UserAdditionalInfoData getUserAdditionalInfoData(Long userId) {
+        return userAdditionalInfoRepository.getUserAdditionalInfoData(userId);
+    }
+}
--- src/main/java/com/example/demo/domain/user_addtional_info/repository/UserAdditionalInfoRepository.java
+++ src/main/java/com/example/demo/domain/user_addtional_info/repository/UserAdditionalInfoRepository.java
@@ -1,12 +1,13 @@
 package com.example.demo.domain.user_addtional_info.repository;
 
-import com.example.demo.domain.user_addtional_info.domain.UserAdditionalInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import com.example.demo.domain.user_addtional_info.entity.UserAdditionalInfoData;
+import com.example.demo.domain.user_addtional_info.vo.StudentStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-public interface UserAdditionalInfoRepository extends JpaRepository<UserAdditionalInfo, Long> {
-    @Modifying
-    @Query("UPDATE UserAdditionalInfo u SET u.isUpdated = false")
-    void updateAllIsUpdatedToFalse();
+public interface UserAdditionalInfoRepository{
+    UserAdditionalInfoData getUserAdditionalInfoData(Long userId);
+    Page<UserAdditionalInfoData> findAll(Pageable pageable);
+    void createUserAdditionalInfo(Long userId, UserAdditionalInfoData request);
+    void updateAcademicInfo(Long userId, int grade, StudentStatus studentStatus);
 }
