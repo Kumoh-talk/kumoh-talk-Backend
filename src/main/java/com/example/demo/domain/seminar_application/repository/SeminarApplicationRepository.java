3999920ce4f4d53dcbe91a3b1ba598685fb39963
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2025-03-18 21:59:32 +0900

    refactor: 세미나 신청 패키지 구조 리팩터링 (도메인 계층)

--- src/main/java/com/example/demo/domain/seminar_application/repository/SeminarApplicationRepository.java
+++ src/main/java/com/example/demo/domain/seminar_application/repository/SeminarApplicationRepository.java
@@ -1,12 +1,25 @@
 package com.example.demo.domain.seminar_application.repository;
 
-import com.example.demo.domain.seminar_application.domain.SeminarApplication;
-import com.example.demo.domain.seminar_application.domain.dto.response.SeminarApplicationInfo;
-import com.example.demo.infra.user.entity.User;
+import com.example.demo.domain.seminar_application.entity.SeminarApplicationInfo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SeminarApplicationRepository extends JpaRepository<SeminarApplication, Long> {
-    Page<SeminarApplicationInfo> findByUser(User user, Pageable pageable);
+import java.util.Optional;
+
+public interface SeminarApplicationRepository {
+    Optional<SeminarApplicationInfo> findById(Long id);
+
+    boolean isFirstApplication(Long userId);
+
+    void addSeminarApplication(Long userId, SeminarApplicationInfo seminarApplicationInfo);
+
+    void updateSeminarApplication(Long id, SeminarApplicationInfo seminarApplicationInfo);
+
+    boolean canEditOrDelete(Long id);
+
+    void deleteById(Long id);
+
+    Page<SeminarApplicationInfo> findByUserId(Long userId, Pageable pageable);
+
+    Page<SeminarApplicationInfo> findAll(Pageable pageable);
 }
