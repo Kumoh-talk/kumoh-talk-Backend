81be42e60164c948548ed8779a74190f84e5f752
siwan9 <si.wan208@gmail.com>
2025-03-21 14:41:51 +0900

    build : prod 환경 적용

--- src/main/resources/db/migration/V20250321134536__test.sql
+++
@@ -1,9 +0,0 @@
-create table test
-(
-    id   int primary key,
-    name varchar(255)
-);
-
-alter table board_comments
-    add column test int;
-alter table board_comments modify column content text default null;
